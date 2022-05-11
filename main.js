e6edf3398db29320a6388c5104e3937215bffccf
QimieGames <45062664+QimieGames@users.noreply.github.com>
2022-05-11 22:36:25 +0800

    Expect a rewrite soon.

--- main.js
+++ main.js
@@ -6,12 +6,6 @@ const YAML = require('js-yaml');
 
 const {Client, Intents, MessageEmbed, Collection} = require('discord.js');
 
-//const { REST } = require('@discordjs/rest');
-
-//const { Routes } = require('discord-api-types/v9');
-
-//const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_BOT_TOKEN);
-
 const mineflayer = require('mineflayer');
 
 const wait = require('node:timers/promises').setTimeout;
@@ -42,46 +36,15 @@ const discordBotPrefix = config.discord_bot.prefix;
 const discordBotAdmin = config.roles_id.admin;
 const discordBotTrusted = config.roles_id.trusted;
 
-/*const commandFiles = readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-const commands = [];
-
-discordBot.commands = new Collection();
-
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
-    discordBot.commands.set(command.data.name, command);
-}*/
-
 console.log("[Discord] Connecting to the Discord bot...");
 
 discordBot.login(process.env.DISCORD_BOT_TOKEN);
-
-/*rest.put(Routes.applicationGuildCommands(config.discord_bot.client_id, config.discord_bot.guild_id), { body: commands })
-	.then(() => console.log('[Discord] Successfully registered slash commands.'))
-	.catch(console.error);
-    
-    (async () => {
-        try {
-            console.log('[Discord] Refreshing slash commands.');
-    
-            await rest.put(
-                Routes.applicationGuildCommands(config.discord_bot.client_id, config.discord_bot.guild_id),
-                { body: commands },
-            );
-    
-            console.log('[Discord] Successfully reloaded slash commands.');
-        } catch (error) {
-            console.error(error);
-        }
-    })();*/
 
 const helpMenuEmbed = new MessageEmbed()
 	.setColor('#25c42b')
 	.setTitle('COMMANDS LIST')
 	.setURL('https://mchub.com/')
-	.setDescription(discordBotPrefix + 'help = Shows this menu.\n\n' + discordBotPrefix + "stats (IGN) = Check a staff member's staffstats.\n\n" + discordBotPrefix + 'vote (IGN) (Rank To Promote/Demote To) = Start a promotion/demotion vote on said staff member. [Admin Command]\n\n' + discordBotPrefix + 'promote (IGN) (Rank To Promote To) = Promote a staff member. [Admin Command]\n\n' + discordBotPrefix + 'demote (IGN) (Rank To Demote To) = Demote a staff member. [Admin Command]\n\n' + discordBotPrefix + 'addstaff (IGN) (Gamemode) (Rank) = Add a new player to the staff team. [Admin Command]\n\n' + discordBotPrefix + "editstaff (IGN) (New IGN) (New Rank) (New Gamemode) = Edit a staff member's details. [Admin Command]\n\n" + discordBotPrefix + 'removestaff (IGN) = Remove a staff member from the staff team. [Admin Command]\n\n' + discordBotPrefix + 'sudo (Command) = Run a command ingame. [Admin Command]\n\n' + discordBotPrefix + 'restart = Restart the bot. [Admin Command]')
+	.setDescription(discordBotPrefix + 'help = Shows this menu.\n\n' + discordBotPrefix + "stats (IGN) = Check a staff member's staffstats.\n\n" + discordBotPrefix + 'vote (IGN) (Rank To Promote/Demote To) = Start a promotion/demotion vote on said staff member. [Admin Command]\n\n' + discordBotPrefix + 'promote (IGN) (Rank To Promote To) = Promote a staff member. [Admin Command]\n\n' + discordBotPrefix + 'demote (IGN) (Rank To Demote To) = Demote a staff member. [Admin Command]\n\n' + discordBotPrefix + 'addstaff (IGN) (Gamemode) (Rank) = Add a new player to the staff team. [Admin Command]\n\n' + discordBotPrefix + 'removestaff (IGN) = Remove a staff member from the staff team. [Admin Command]\n\n' + discordBotPrefix + 'sudo (Command) = Run a command ingame. [Admin Command]\n\n' + discordBotPrefix + 'restart = Restart the bot.')
 	.setThumbnail('https://i.imgur.com/3ovjQst.png')
 	.setTimestamp()
 	.setFooter({text: 'Custom Coded By QimieGames', iconURL: 'https://i.imgur.com/io8hIIm.png'});
@@ -90,7 +53,7 @@ function loadIndividualStaffData(staff){
     let individualStaffData = JSON.parse(readFileSync(process.env.INDIVIDUAL_STAFF_DIR + `${staff}.json`).toString());
     let updatedStaffsList = [];
     console.log(`[Data Manager] Loading staff data for ${staff}...`);
-    if(Number.isInteger(individualStaffData.staff_rank) === true && individualStaffData.ingame_name === staff && individualStaffData.staff_rank <= 4 && individualStaffData.staff_rank >= 1 && individualStaffData.gamemode >= 1 && individualStaffData.gamemode <= 4){
+    if(Number.isInteger(individualStaffData.staff_rank) === true && individualStaffData.ingame_name === staff && individualStaffData.staff_rank <= 3 && individualStaffData.staff_rank >= 0 && individualStaffData.gamemode >= 0 && individualStaffData.gamemode <= 3){
         updatedStaffsList.push(`${staff}`);
         console.log(`[Data Manager] Successfully loaded staff data for ${staff}!`);
     } else {
@@ -226,38 +189,37 @@ discordBot.on('messageCreate', discordMessage => {
 
     switch(discordCommand){
         default:
-                discordMessage.reply("```Invalid Command. Do " + discordBotPrefix + "help for list of commands.```");
+            discordMessage.reply("```Invalid Command. Do " + discordBotPrefix + "help for list of commands.```");
             break;
         case 'help':
             if(args[0]){
                 discordMessage.reply("```Invalid Command Usage. Usage: " + discordBotPrefix + "help```");
             } else {
-                    discordMessage.reply({embeds: [helpMenuEmbed]});
+                discordMessage.reply({embeds: [helpMenuEmbed]});
             }
             break;
         case 'addstaff':
-            if(!args[0] || !args[1] || !args[2] || args[3]){
+            if(!args[2] || args[3]){
                 discordMessage.reply("```Invalid Command Usage. Usage: " + discordBotPrefix + "addstaff (IGN) (Gamemode) (Rank)```");
             } else {
-                if(discordMessage.member.roles.cache.has(`${discordBotAdmin}`) === true){
+                if(discordMessage.member.roles.cache.has(`${discordBotAdmin}`) === false){
+                    discordMessage.reply("```You are not allowed to run this command.```");
+                } else {
                     if(staffsList.find(staff => staff === args[0]) === args[0]){
-                        discordMessage.reply(args[0] + " is already on the staff team.");
+                        discordMessage.reply("```" + args[0] + " is already on the staff team.```");
                     } else {
                         const validGamemodeOptions = ["Prisons", "Skyblock", "Survival", "Global"];
                         if(validGamemodeOptions.find(gamemodeOptions => gamemodeOptions === args[1]) !== args[1]){
-                            discordMessage.reply("Invalid Gamemode. Gamemodes: Prisons/Skyblock/Survival/Global");
+                            discordMessage.reply("```Invalid Gamemode. Gamemodes: Prisons/Skyblock/Survival/Global```");
                         } else {
                             const validStaffRanks = ["Trials", "Helper", "Mod", "SrMod"];
                             if(validStaffRanks.find(staffRankOptions => staffRankOptions === args[2]) !== args[2]){
-                                discordMessage.reply("Invalid Staff Rank. Staff Ranks: Trials/Helper/Mod/SrMod");
+                                discordMessage.reply("```Invalid Staff Rank. Staff Ranks: Trials/Helper/Mod/SrMod```");
                             } else {
-                                addStaff(args[0], args[1], args[2]);
-                                discordMessage.reply("Added " + args[0] + " to the staff team as " + args[1] + " " + args[2] + ".");
+                                //IMPLEMENT BETTER SYSTEM
                             }
                         }
                     }
-                } else {
-                    discordMessage.reply("```You are not allowed to run this command.```");
                 }
             }
             break;
@@ -265,15 +227,14 @@ discordBot.on('messageCreate', discordMessage => {
             if(!args[0] || args[1]){
                 discordMessage.reply("```Invalid Command Usage. Usage: " + discordBotPrefix + "removestaff (IGN)```");
             } else {
-                if(discordMessage.member.roles.cache.has(`${discordBotAdmin}`) === true){
-                    if(staffsList.find(staff => staff === args[0]) === args[0]){
-                        removeStaff(args[0]);
-                        discordMessage.reply("Removed " + args[0] + " from the staff team.");
-                    } else {
-                        discordMessage.reply(args[0] + " is not on the staff team.");
-                    }
-                } else {
+                if(discordMessage.member.roles.cache.has(`${discordBotAdmin}`) === false){
                     discordMessage.reply("```You are not allowed to run this command.```");
+                } else {
+                    if(staffsList.find(staff => staff === args[0]) !== args[0]){
+                        discordMessage.reply("```" + args[0] + " is not on the staff team.");
+                    } else {
+                        //IMPLEMENT BETTER SYSTEM
+                    }
                 }
             }
             break;
@@ -281,12 +242,12 @@ discordBot.on('messageCreate', discordMessage => {
             if(args[0]){
                 discordMessage.reply("```Invalid Command Usage. Usage: " + discordBotPrefix + "restart```");
             } else {
-                if(discordMessage.member.roles.cache.has(`${discordBotAdmin}`) === true){
+                if(discordMessage.member.roles.cache.has(`${discordBotAdmin}`) === false || discordMessage.member.roles.cache.has(`${discordBotTrusted}`) === false){
+                    discordMessage.reply("```You are not allowed to run this command.```");
+                } else {
                     discordMessage.reply("```Restarting...```").then(() => {
                         process.exit(0);
                     });
-                } else {
-                    discordMessage.reply("```You are not allowed to run this command.```");
                 }
             }
             break;
@@ -294,15 +255,3 @@ discordBot.on('messageCreate', discordMessage => {
 
 
 });
-
-/*discordBot.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-    const command = discordBot.commands.get(interaction.commandName);
-    if (!command) return;
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        if (error) console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
-});*/
